22092b6d30c79134a8a99f5788a03a3b16d45bad
Julvan <julivandamaceno@gmail.com>
2016-07-11 00:05:57 -0300

    adding dependencies and configuring js stack

---
+++ assets/javascripts/main.js
@@ -0,0 +1,30 @@
+const velocity = require("velocity-animate");
+
+(function () {
+
+  const CLOSE_BUTTON = document.getElementById('js-contextual-menu-see-more');
+
+  CLOSE_BUTTON.addEventListener('click', (e) => {
+    const BUTTON = e.target;
+    const PLUS_TEXT = 'VER +';
+    const MINUS_TEXT = 'VER -'
+    const LIST_ITEMS = BUTTON.parentNode.querySelectorAll('.crui-menu--list--item__hidden');
+
+    e.preventDefault();
+
+    if (BUTTON.classList.contains('opened')) {
+      BUTTON.innerHTML = PLUS_TEXT;
+      BUTTON.classList.remove('opened');
+
+      Velocity(LIST_ITEMS, 'fadeOut');
+
+      return;
+    }
+
+    BUTTON.classList.add('opened');
+    BUTTON.innerHTML = MINUS_TEXT;
+
+    Velocity(LIST_ITEMS, 'fadeIn');
+  });
+
+}());
--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,63 @@
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 const imagemin = require('gulp-imagemin');
+const gutil = require('gulp-util');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const watchify = require('watchify');
+const notify = require('gulp-notify');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
+
+  notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>'
+  }).apply(this, args);
+
+  this.emit('end');
+}
+
+function buildScript(file, watch) {
+  var props = {
+    entries: ['./assets/javascripts/' + file],
+    debug : false,
+    transform: [babelify]
+  };
+
+  // watchify() if watch requested, otherwise run browserify() once
+  var bundler = watch ? watchify(browserify(props)) : browserify(props);
+
+  function rebundle() {
+    var stream = bundler.bundle();
+
+    return stream
+      .on('error', handleErrors)
+      .pipe(source(file))
+      .pipe(gulp.dest('./assets/dist/javascripts'))
+      // .pipe(buffer())
+      // .pipe(uglify())
+      // .pipe(rename('app.min.js'))
+      // .pipe(gulp.dest('./build'))
+      .pipe(reload({stream:true}))
+  }
+
+  // listen for an update and run rebundle
+  bundler.on('update', function() {
+    rebundle();
+    gutil.log('Rebundle...');
+  });
+
+  // run it once the first time buildScript is called
+  return rebundle();
+}
 
 gulp.task('imagemin', function() {
-    return gulp.src(['./assets/images/*', '!./assets/images/dist'])
+    return gulp.src(['./assets/images/*'])
       .pipe(imagemin({
           progressive: true
       }))
@@ -20,6 +74,11 @@ gulp.task('stylus', () => {
     .pipe(gulp.dest('./assets/dist/css'));
 });
 
+gulp.task('scripts', function() {
+  return buildScript('main.js', false);
+});
+
 gulp.task('default', () => {
   gulp.watch('./assets/stylus/**/*.styl', ['stylus']);
+  return buildScript('main.js', true)
 });
